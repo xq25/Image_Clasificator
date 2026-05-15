8334e573e9d674ef82b415a6ef897d9a72122c28
xq25 <jacoboq92@gmail.com>
2026-05-14 22:53:47 -0500

    Desarrollo pagina validate2FA

--- src/app/pages/authentication/authentication.routes.ts
+++ src/app/pages/authentication/authentication.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { AppSideLoginComponent } from './side-login/side-login.component';
 import { AppSideRegisterComponent } from './side-register/side-register.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { Validate2FAComponent } from './validate2-fa/validate2-fa.component';
 
 export const AuthenticationRoutes: Routes = [
   {
@@ -23,6 +24,14 @@ export const AuthenticationRoutes: Routes = [
       {
         path: 'forgot-password/:token',
         component: ForgotPasswordComponent,
+      },
+      {
+        path: 'validate2FA',
+        component: Validate2FAComponent,
+      },
+      {
+        path: 'validate2-fa',
+        component: Validate2FAComponent,
       },
     ],
   },
