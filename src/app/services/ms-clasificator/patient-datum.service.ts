c9b116dbdcd2a839421460210a9f8a3b86527152
xq25 <jacoboq92@gmail.com>
2026-06-12 18:31:31 -0500

    Version corregida 1

--- src/app/services/ms-clasificator/patient-datum.service.ts
+++ src/app/services/ms-clasificator/patient-datum.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { PatientDatum, PatientDatumExtended, ApiResponse } from '@models/ms-clasificator';
+import { ApiResponse } from '@models/ms-clasificator';
+import { PatientDatum, PatientDatumExtended } from '@app/models/ms-clasificator/PatientDatum/PatientDatum';
 
 const apiUrl = `${environment.url_backend_clasificator}/api/patient-datums`;
 
