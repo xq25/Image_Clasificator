6d9181c4a1e7feb7e569dc07a70240162935d5e9
xq25 <jacoboq92@gmail.com>
2026-06-13 09:32:26 -0500

    Correccion de importacion

--- src/app/services/ms-clasificator/diagnosis.service.ts
+++ src/app/services/ms-clasificator/diagnosis.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { Diagnosis, DiagnosisExtended, ApiResponse } from '@models/ms-clasificator';
+import { ApiResponse } from '@models/ms-clasificator';
+import { Diagnosis, DiagnosisExtended } from '@models/ms-clasificator/Diagnosis/Diagnosis';
 
 const apiUrl = `${environment.url_backend_clasificator}/api/diagnosis`;
 
